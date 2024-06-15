45908abc7211c868f6445bc68f4a65e4ddcd6555
Andres <andresalvarez360@gmail.com>
2024-06-14 20:02:43 -0400

    Agregando set up

--- src/test/java/fizz_buzz/FizzBuzzTest.java
+++ src/test/java/fizz_buzz/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package fizz_buzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,40 +8,52 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+
+    private String result;
+
+    @BeforeEach
+    void setUp() {
+        result = null;
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 12})
     @DisplayName("Fizz numbers")
     void testFizz(int number) {
-        String result = FizzBuzz.fizzBuzz(number);
+        result = FizzBuzz.fizzBuzz(number);
         assertEquals("Fizz", result, getMessage(number, result, "Fizz"));
     }
+
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 15, 20})
     @DisplayName("Buzz numbers")
     void testBuzz(int number) {
-        String result = FizzBuzz.fizzBuzz(number);
+        result = FizzBuzz.fizzBuzz(number);
         assertEquals("Buzz", result,getMessage(number, result, "Buzz"));
     }
+
     @ParameterizedTest
     @ValueSource(ints = {15, 30, 45, 60})
     @DisplayName("FizzBuzz numbers")
     void testFizzBuzz(int number) {
-        String result = FizzBuzz.fizzBuzz(number);
+        result = FizzBuzz.fizzBuzz(number);
         assertEquals("FizzBuzz", result,getMessage(number, result, "FizzBuzz"));
     }
+
     @ParameterizedTest
     @ValueSource(ints = {1, 17, 47, 88})
     @DisplayName("Other numbers")
     void testOther(int number) {
-        String result = FizzBuzz.fizzBuzz(number);
+        result = FizzBuzz.fizzBuzz(number);
         String expected = String.valueOf(number);
         assertEquals(expected, result,getMessage(number, result, expected));
     }
+
     @ParameterizedTest
     @ValueSource(ints = {0, -1, 101})
     @DisplayName("Out of range numbers")
     void testOutOfRange(int number) {
-        String result = FizzBuzz.fizzBuzz(number);
+        result = FizzBuzz.fizzBuzz(number);
         String expected = "Out of range";
         assertEquals(expected, result,getMessage(number, result, expected));
     }
